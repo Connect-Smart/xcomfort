7e79aa525ba924942460564f905230cd063db5d9
Jos Rothman <jos@rothman.nl>
2023-11-17 12:26:05 +0100

    update

--- custom_components/xcomfort/light.py
+++ custom_components/xcomfort/light.py
@@ -92,8 +92,6 @@ class xcLight(LightEntity):
 
     async def async_turn_on(self, **kwargs):
         if ATTR_BRIGHTNESS in kwargs:
-            brightness = int(100 * kwargs.get(ATTR_BRIGHTNESS, 255) / 255)
-            _LOGGER.debug("xcLight brightness: %s", brightness)
             brightness = int(kwargs.get(ATTR_BRIGHTNESS, self.stored_brightness))
             _LOGGER.debug("xcLight brightness: %s", brightness)
             if await self.coordinator.xc.switch(self._unique_id, str(brightness)):
