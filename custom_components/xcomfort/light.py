174b6ee1733047ca04e2880a3c0f24bcc4eaa259
Jos Rothman <jos@rothman.nl>
2023-11-17 16:43:47 +0100

    upate

--- custom_components/xcomfort/light.py
+++ custom_components/xcomfort/light.py
@@ -110,8 +110,8 @@ class xcLight(LightEntity):
     async def async_turn_off(self, **kwargs):
         if self.type == 'DimActuator':
             if await self.coordinator.xc.switch(self._unique_id,"off"):
-                self.coordinator.data[self.id]['value']='0'
-                await self.async_update_ha_state()
+                #self.coordinator.data[self.id]['value']='0'
+                #await self.async_update_ha_state()
                 _LOGGER.debug("xcLight.turn_off dimm %s success",self.name)
             else:
                 _LOGGER.debug("xcLight.turn_on dimm %s unsucessful",self.name)
